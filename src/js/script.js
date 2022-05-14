18f50794bc69cc9447493fdb6b727d50dec6c26f
abheetha7D0 <abeetha.dilushan@gmail.com>
2022-05-14 23:32:12 +0530

    first showing img and flipping

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,52 @@
 const card = document.querySelectorAll('.cell')
+const front = document.querySelectorAll('.front')
 
 suffleImage()
-function suffleImage() {
-    card.forEach(c => {
+clicking()
+function suffleImage(){
+    card.forEach(c=>{
         const num = [...Array(card.length).keys()]
-        const random = Math.floor(Math.random() * card.length)
-
+        const random = Math.floor(Math.random()*card.length)
         c.style.order = num[random]
     })
 }
 
-function clicking(){
-for(let i=0; i<card.length; i++){
-card[i].addEventListener('click',()=>{
-    front[i].classlist.add('flip')
-})
 
+function clicking(){
+
+    for(let i =0; i<card.length; i++){
+        front[i].classList.add('show')
+
+        setInterval(() => {
+            front[i].classList.remove('show')
+        }, 2000);
+
+        card[i].addEventListener('click' ,()=>{
+            front[i].classList.add('flip')
+           const filppedCard = document.querySelectorAll('.flip')
+
+            if(filppedCard.length == 2){
+                match(filppedCard[0] , filppedCard[1])
+            }
+
+        })
+    }
 }
 
+function math(cardOne, cardTwo) {
+    if(cardOne.dataset.index == cardTwo.dataset.index){
+       
+        cardOne.classList.remove('flip') 
+        cardTwo.classList.remove('flip') 
+
+        cardOne.classList.add('match')
+        cardTwo.classList.add('match')
+
+    }else{
+
+       setTimeout(() => {
+            cardOne.classList.remove('flip') 
+            cardTwo.classList.remove('flip') 
+        }, 1000);
+    }
 }
